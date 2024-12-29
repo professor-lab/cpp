a9df7acb08c1bedbed52190bd4c284e5450ef3cb
Environment-Code <arjansambad@gmail.com>
2024-12-29 10:38:17 +0530

    function overloading

---
+++ function/function-overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//prototype (declaration)
+int volume(int);
+double volume(double,int);
+long volume(long,int,int);
+int main()
+{
+    cout<<volume(10)<<"\n";
+    cout<<volume(2.5,8)<<"\n";
+    cout<<volume(100,75,15)<<"\n";
+    return 0;
+}
+//fnction definitions
+int volume(int s)//cube
+{
+    return(s*s*s);
+}
+double volume(double r,int h)//cylinder
+{
+    return(3.14519*r*r*h);
+}
+long volume(long l,int b,int h) //rectangular box
+{
+    return(l*b*h);
+}
