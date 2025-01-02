3f315119dc966e41edf2f73915e18415b602ffe3
cpp-professor <arjansambad@gmail.com>
2025-01-02 16:05:34 +0530

    OBJECT AA S FUNCTION ARGUMENT

---
+++ class-object/object-as-function-args.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class time
+{
+        int hours;
+        int minutes;
+    public:
+        void gettime(int h,int m)
+        {
+            hours=h;
+            minutes=m;
+        }
+        void puttime(void)
+        {
+            cout<<hours<<"Hours and ";
+            cout<<minutes<<"minutes "<<"\n";
+        }
+        void sum(time,time);  //declration with object 
+};
+void time::sum(time t1,time t2)
+{
+    minutes=t1.minutes+t2.minutes;
+    hours=minutes/60;
+    minutes=minutes%60;
+    hours=hours+t1.hours+t2.hours;
+}
+int main()
+{
+    time T1,T2,T3;
+
+    T1.gettime(2,45);
+    T2.gettime(3,30);
+
+    T3.sum(T1,T2);
+
+    cout<<"t1 = ";
+    T1.puttime();
+    cout<<"t2 = ";
+    T2.puttime();
+    cout<<"t3 = ";
+    T3.puttime();
+
+    return 0;
+}
