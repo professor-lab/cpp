16f418244e03c2a20b52801ecca3184e2b4e6154
cpp-professor <arjansambad@gmail.com>
2025-01-03 10:54:45 +0530

    friend function final update

--- class-object/friend-function.cpp
+++ class-object/friend-function.cpp
@@ -1,1 +1,28 @@
 #include<iostream>
+using namespace std;
+
+class sample
+{
+private:
+    int a;
+    int b;
+public:
+    void setvalue()
+    {
+        a=25;
+        b=40;
+    }
+    friend float mean(sample s);
+};
+float mean(sample s)
+{
+    return float(s.a + s.b)/2.0;
+}
+int main()
+{
+    sample x; //object x
+    x.setvalue();
+
+    cout<<"Main value = "<<mean(x)<<"\n";
+    return 0;
+}
