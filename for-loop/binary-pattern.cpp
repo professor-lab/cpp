1798a9524bda35e052c52639a8634ed8656eec77
professor-lab <arjansambad@gmail.com>
2025-03-01 10:42:03 +0530

    binary pattern

---
+++ for-loop/binary-pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=10;i++){
+        int start=(i%2)?1:0;
+        for(int j=1;j<=i;j++){
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
